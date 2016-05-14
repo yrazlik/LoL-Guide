2957310c6db6dc88b26a792565c47088f857e67f
Yasin Razlik <yrazlik@ebay.com>
2016-05-14 19:36:31 +0300

    icons ctd

--- app/src/main/java/com/yrazlik/loltr/activities/PlayerMatchInfoDetailActivity.java
+++ app/src/main/java/com/yrazlik/loltr/activities/PlayerMatchInfoDetailActivity.java
@@ -216,7 +216,7 @@ public class PlayerMatchInfoDetailActivity extends ActionBarActivity implements 
         }else if(league.equalsIgnoreCase("CHALLENGER")){
             return R.drawable.challenger_badge;
         }else if(league.equalsIgnoreCase("MASTER")){
-            return R.drawable.challenger_badge;
+            return R.drawable.master_badge;
         }else{
             return R.drawable.unranked_badge;
         }
