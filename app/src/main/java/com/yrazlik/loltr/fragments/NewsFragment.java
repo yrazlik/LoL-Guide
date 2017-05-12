8264594676268640c76913728be19a37fa89f456
Yasin Razlik <yrazlik@ebay.com>
2017-05-12 11:30:16 +0300

    news fix rollback

--- app/src/main/java/com/yrazlik/loltr/fragments/NewsFragment.java
+++ app/src/main/java/com/yrazlik/loltr/fragments/NewsFragment.java
@@ -12,9 +12,6 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
-import com.google.android.gms.ads.formats.NativeAd;
-import com.google.android.gms.ads.formats.NativeAppInstallAd;
-import com.google.android.gms.ads.formats.NativeContentAd;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 import com.yrazlik.loltr.LolApplication;
@@ -23,9 +20,6 @@ import com.yrazlik.loltr.adapters.NewsAdapter;
 import com.yrazlik.loltr.commons.Commons;
 import com.yrazlik.loltr.data.News;
 import com.yrazlik.loltr.listener.ResponseListener;
-import com.yrazlik.loltr.utils.AdUtils;
-import com.yrazlik.loltr.utils.LocalizationUtils;
-import com.yrazlik.loltr.utils.NativeAdLoader;
 import com.yrazlik.loltr.view.RobotoTextView;
 
 import java.util.ArrayList;
@@ -103,7 +97,6 @@ public class NewsFragment extends BaseFragment implements ResponseListener, Adap
                                 noRecentNewsTV.setVisibility(View.GONE);
                                 news = sortByDateCreated(news);
                                 Collections.reverse(news);
-                                addAdsToNewsArray();
                                 adapter = new NewsAdapter(getActivity(), R.layout.list_row_news, news);
                                 newsLV.setAdapter(adapter);
                             } else {
@@ -133,23 +126,12 @@ public class NewsFragment extends BaseFragment implements ResponseListener, Adap
         }
     }
 
-    private void addAdsToNewsArray() {
-        NativeAd nativeAd = AdUtils.getInstance().getCachedAd();
-        if(nativeAd != null) {
-            News ad = new News();
-            ad.setAd(true);
-            ad.setNativeAd(nativeAd);
-            try {
-                news.add(0, ad);
-            } catch (Exception ignored) {}
-            notifyDataSetChanged();
-        }
-    }
-
-    private void notifyDataSetChanged() {
-        if (adapter != null) {
-            adapter.notifyDataSetChanged();
-        }
+    private void showInterstitial(){
+        try {
+            if (((LolApplication) (getActivity().getApplication())).shouldShowInterstitial()) {
+                ((LolApplication) (getActivity().getApplication())).showInterstitial();
+            }
+        }catch (Exception ignored){}
     }
 
     private ArrayList<News> sortByDateCreated(ArrayList<News> news){
@@ -170,15 +152,39 @@ public class NewsFragment extends BaseFragment implements ResponseListener, Adap
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         try {
             News selectedNews = news.get(position);
-            if(!selectedNews.isAd()) {
-                Commons.openInBrowser(getContext(), selectedNews.getUrl());
+
+
+
+         /*   FragmentManager fm = getFragmentManager();
+            Bundle args = new Bundle();
+            if (selectedNews.getTitle() != null && selectedNews.getTitle().length() > 0) {
+                args.putString(NewsDetailFragment.EXTRA_TITLE, selectedNews.getTitle());
+            } else if (selectedNews.getTitleEnglish() != null && selectedNews.getTitleEnglish().length() > 0) {
+                args.putString(NewsDetailFragment.EXTRA_TITLE, selectedNews.getTitleEnglish());
             }
+
+            if (selectedNews.getShortDesc() != null && selectedNews.getShortDesc().length() > 0) {
+                args.putString(NewsDetailFragment.EXTRA_MESSAGE, selectedNews.getShortDesc());
+            } else if (selectedNews.getShortDescEnglish() != null && selectedNews.getShortDescEnglish().length() > 0) {
+                args.putString(NewsDetailFragment.EXTRA_MESSAGE, selectedNews.getShortDescEnglish());
+            }
+            args.putString(NewsDetailFragment.EXTRA_IMAGE_URL, selectedNews.getLargeImage());
+            args.putString(NewsDetailFragment.EXTRA_WV_URL, selectedNews.getVideoUrl());
+            NewsDetailFragment fragment = new NewsDetailFragment();
+            fragment.setArguments(args);
+            FragmentTransaction ft = fm.beginTransaction();
+            Commons.setAnimation(ft, Commons.ANIM_OPEN_FROM_RIGHT_WITH_POPSTACK);
+            ft.replace(R.id.content_frame, fragment).addToBackStack(Commons.NEWS_DETAIL_FRAGMENT).commit();
+            showInterstitial();*/
+
+            Commons.openInBrowser(getContext(), selectedNews.getUrl());
+
         } catch (Exception ignored) {
         }
     }
 
     public String getLocalizedNews() {
-        return "news" + "-" +  LocalizationUtils.getInstance().getLocale().toLowerCase();
+        return "news" + "-" +  Commons.getLocale().toLowerCase();
     }
 
     @Override
